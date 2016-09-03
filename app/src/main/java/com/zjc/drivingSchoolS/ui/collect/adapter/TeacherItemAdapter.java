6a0aa65063bb92ffacc9c80a1bf8a2604f30c175
1020526698@qq.com <zjcx740811016>
2016-09-03 21:03:07 +0800

    1.编写查询教练功能。

--- app/src/main/java/com/zjc/drivingSchoolS/ui/collect/adapter/TeacherItemAdapter.java
+++ app/src/main/java/com/zjc/drivingSchoolS/ui/collect/adapter/TeacherItemAdapter.java
@@ -17,6 +17,7 @@ import com.zjc.drivingSchoolS.utils.Constants;
  * Created by Administrator on 2016/8/18.
  */
 public class TeacherItemAdapter extends ZBaseRecyclerViewAdapter {
+
     public TeacherItemAdapter(Context context) {
         super(context);
     }
@@ -30,15 +31,17 @@ public class TeacherItemAdapter extends ZBaseRecyclerViewAdapter {
     class CollectManagerFrgViewHolder extends BaseViewHolder<TeacherItem> {
         private TextView tvName;
         private TextView tvContent;
+        private TextView tvDistribution;
         private RatingBar rbScore;
         private SimpleDraweeView mImage;
 
         public CollectManagerFrgViewHolder(ViewGroup parent) {
-            super(parent, R.layout.collect_manager_adapter);
+            super(parent, R.layout.teacher_item);
             rbScore = $(R.id.assess_detail_list_rb_score);
             mImage = $(R.id.personal_center_img_person);
             tvName = $(R.id.collect_name);
             tvContent = $(R.id.collect_content);
+            tvDistribution = $(R.id.receive_item_tv_distribution);
         }
 
         @Override
@@ -47,8 +50,12 @@ public class TeacherItemAdapter extends ZBaseRecyclerViewAdapter {
             //设置评分条是否只能看，不能改
             rbScore.setIsIndicator(true);
             rbScore.setRating(Float.parseFloat(String.valueOf(service.getStars())));
-            tvContent.setText(service.getTeachername());
             tvName.setText(service.getTeachername());
+            if (service.isOrderswitch()) {
+                tvContent.setText("接单中");
+            } else {
+                tvContent.setText("休息中");
+            }
         }
     }
 }
